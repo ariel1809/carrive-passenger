c879ee3ad57387ecead46440516252914db5ef36
ariel <Naruto18@>
2024-11-01 13:44:23 +0100

    init passenger

--- src/main/java/com/manage/carrivepassenger/CarrivePassengerApplication.java
+++ src/main/java/com/manage/carrivepassenger/CarrivePassengerApplication.java
@@ -2,8 +2,14 @@ package com.manage.carrivepassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = {"com.manage.carriveutility.repository"})
+@EntityScan("com.manage.carrive")
+@EnableFeignClients
 public class CarrivePassengerApplication {
 
     public static void main(String[] args) {
