cdf1381ea154609652f9ab6e616f57beb0c26d10
ariel <Naruto18@>
2024-11-01 13:23:30 +0100

    init passenger

---
+++ src/main/java/com/manage/carrivepassenger/CarrivePassengerApplication.java
@@ -0,0 +1,13 @@
+package com.manage.carrivepassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrivePassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrivePassengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manage/carrivepassenger/CarrivePassengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.carrivepassenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrivePassengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
